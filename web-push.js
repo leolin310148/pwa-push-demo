198f0d24a6e0225038fb313c6b60fb96731ba179
leolin310148 <leolin310148@gmail.com>
2023-09-22 09:19:41 +0000

    Deploying to gh-pages from @ leolin310148/pwa-push-demo@d7669e3f1a8e2ea01c74cb246ab664079a74d160 🚀

---
+++ web-push.js
@@ -0,0 +1,36 @@
+const config = {
+    pushKey:
+        "BK4r62oUQj7Uhr2FAHin3h1Vqvv0zkC_XZwM9Ej0hIwK216Gins_Ye-2Ju_RufJWTgiX2d7zcHCSqY8frmTjPjQ",
+    appSyncUrl:
+        "https://axd7bfjqezhupnepjttv5ylmo4.appsync-api.ap-northeast-1.amazonaws.com/graphql",
+    appSyncApiKey: "da2-tbjj4nm6njghzekqcatk3yw7wm",
+};
+async function subscribe(topic) {
+    swReg = await navigator.serviceWorker.register("/pwa-push-demo/service-worker.js");
+    const subscription = await swReg.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlB64ToUint8Array(config.pushKey),
+    });
+
+    fetch(config.appSyncUrl, {
+        method: "POST",
+        headers: { "x-api-key": config.appSyncApiKey },
+        body: JSON.stringify({ query: `mutation($topic: String, $subscription: String) {subscribe(topic: $topic, subscription: $subscription)}`,
+            variables: { topic, subscription: JSON.stringify(subscription) } })
+    });
+}
+function urlB64ToUint8Array(base64String) {
+    const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, "+")
+        .replace(/_/g, "/");
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+subscribe("news");
