20efb8c3a3a1cf15dbd1912c9a0b998006b85c3b
Leo <leo.lin@nttdata.com>
2023-09-25 17:45:17 +0800

    chore: log

--- pwa-push-demo/service-worker.js
+++ pwa-push-demo/service-worker.js
@@ -1,8 +1,3 @@
-if ('function' === typeof importScripts) {
-    importScripts("https://js.pusher.com/beams/service-worker.js");
-}
-
-
 self.addEventListener("push", function (event) {
     const message = event.data.json();
     self.registration.showNotification( message.title, { body: message.text });
